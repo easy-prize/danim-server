92da476f9a37ec957201ceab52c66766c2329ceb
Joosung Park <admin@slowmotion.dev>
2019-12-21 23:11:57 +0900

    Add: Course

---
+++ libs/course/src/course.class.ts
@@ -0,0 +1,11 @@
+import { ObjectId } from 'mongodb';
+import { ICourse } from './course.interface';
+
+export class Course implements ICourse {
+  public author: ObjectId;
+  public body: string;
+  public isPublished: string;
+  public review: ObjectId[];
+  public thumbnail: string;
+  public title: string;
+}
--- libs/course/src/course.interface.ts
+++ libs/course/src/course.interface.ts
@@ -1,5 +1,10 @@
+import { ObjectId } from 'mongodb';
+
 export interface ICourse {
   title: string;
   isPublished: string;
-
+  author: ObjectId;
+  body: string;
+  review: ObjectId[];
+  thumbnail: string;
 }
